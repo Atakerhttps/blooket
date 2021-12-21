67208ba910419f6b3c86ad9f3f6d631fb000fd92
Ataker <96275397+Atakerhttps@users.noreply.github.com>
2021-12-21 07:54:31 -0500

    Update Tokens and HP.js

--- Tokens and HP.js
+++ Tokens and HP.js
@@ -9,7 +9,7 @@ async function addCurrencies() {
     const tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
     const myToken = localStorage.token.split('JWT ')[1];
 
-    if (tokens > 500) {
+    if (tokens < 500) {
         alert('You can only add up to 500 tokens daily.')
     }
 
